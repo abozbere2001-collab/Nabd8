4a90b27c0f7a94c5ad505f26e251f68be07feb97
عبد السلام <abozbere2001@gmail.com>
2025-10-15 20:31:03 +0000

    لا زالت التبويبات العلويه مثل نتائج مباشر التوقعات هذه في المباريات وفي

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -30,13 +30,13 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "relative inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-foreground",
+      "relative inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-transparent data-[state=active]:text-primary data-[state=active]:shadow-none",
       className
     )}
     {...props}
   >
     {props.children}
-    <div className="absolute bottom-[-1px] h-0.5 w-full scale-x-0 rounded-full bg-primary transition-transform duration-300 data-[state=active]:scale-x-100" />
+     <div className="absolute bottom-[-6px] h-1 w-full scale-x-0 rounded-full bg-primary transition-transform duration-300 data-[state=active]:scale-x-100" />
   </TabsPrimitive.Trigger>
 ))
 TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
