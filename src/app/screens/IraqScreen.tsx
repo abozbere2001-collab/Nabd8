576179170adccebcaa70b8e8b41def4598c34f7d
عبد السلام <abozbere2001@gmail.com>
2025-10-18 10:41:51 +0000

    قبل ان ارسل لك الخطا هناك خطا يمنع قسم كرتنا في تبويب العراق من الفتح Re

--- src/app/screens/IraqScreen.tsx
+++ src/app/screens/IraqScreen.tsx
@@ -29,6 +29,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { useToast } from '@/hooks/use-toast';
 import { hardcodedTranslations } from '@/lib/hardcoded-translations';
