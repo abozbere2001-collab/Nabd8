f5e1da27f03162fa42cca63adf89c0814a4c8f7e
عبد السلام <abozbere2001@gmail.com>
2025-10-24 07:29:52 +0000

    الان فهمت الذي اختفى ليس زر التاج وليس زر تحديث الصداره الذي اختفى هو صل

--- src/firebase/provider.tsx
+++ src/firebase/provider.tsx
@@ -16,7 +16,7 @@ import { errorEmitter } from './error-emitter';
 // Add UIDs of permanent admins here. This provides a fallback if the admins collection is not accessible.
 const ADMIN_UIDS = [
     'qptIHofUmhVs5qGCxDiIfIyqBck1', // abozbere2001@gmail.com
-    'SAGRALNAREY_UID_PLACEHOLDER', // sagralnarey@gmail.com - REPLACE WITH ACTUAL UID
+    '1Jby696bGzaqiODURITOHMr6x552', // sagralnarey@gmail.com
 ];
 
 
