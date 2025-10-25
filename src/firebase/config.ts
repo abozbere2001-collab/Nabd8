dd984fdd783aaf4708c7dc167bc492fdcca0329a
عبد السلام <abozbere2001@gmail.com>
2025-10-25 08:31:28 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDG7nLIqi2QjPqsFGLeLJFHTkrpYbx4

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,10 @@
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
+  apiKey: "AIzaSyDG7nLIqi2QjPqsFGLeLJFHTkrpYbx4sNk",
   authDomain: "studio-5560913388-9a637.firebaseapp.com",
   projectId: "studio-5560913388-9a637",
-  storageBucket: "studio-5560913388-9a637.appspot.com",
-  messagingSenderId: "5560913388",
-  appId: "1:5560913388:web:1234567890abcdef123456",
-  measurementId: "G-MEASUREMENTID"
+  storageBucket: "studio-5560913388-9a637.firebasestorage.app",
+  messagingSenderId: "954115183601",
+  appId: "1:954115183601:web:e979508da0aa1de5506a50"
 };
