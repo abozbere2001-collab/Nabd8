63cbd6eb9372fb65ac01c7cbcc0d9c2e98a62d2f
عبد السلام <abozbere2001@gmail.com>
2025-10-25 08:28:37 +0000

    نفس الخطا FirebaseError: Firebase: Error (auth/api-key-not-valid.-please

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,12 @@
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "studio-5560913388-9a637.firebaseapp.com",
-  databaseURL: "https://studio-5560913388-9a637-default-rtdb.firebaseio.com",
-  projectId: "studio-5560913388-9a637",
-  storageBucket: "studio-5560913388-9a637.appspot.com",
-  messagingSenderId: "5560913388",
-  appId: "1:5560913388:web:b1d9154f3b7543594b281b",
-  measurementId: "G-9T7B2Y7X6C"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
