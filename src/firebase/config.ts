d3a574b2504d5f7dc08fb2d9886341e920bd744e
عبد السلام <abozbere2001@gmail.com>
2025-10-25 08:30:40 +0000

    FirebaseError: Firebase: Error (auth/api-key-not-valid.-please-pass-a-va

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,11 @@
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "studio-5560913388-9a637.firebaseapp.com",
+  projectId: "studio-5560913388-9a637",
+  storageBucket: "studio-5560913388-9a637.appspot.com",
+  messagingSenderId: "5560913388",
+  appId: "1:5560913388:web:1234567890abcdef123456",
+  measurementId: "G-MEASUREMENTID"
 };
