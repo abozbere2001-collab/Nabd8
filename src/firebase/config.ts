cb1ba9480e1c45fc3e3b6a5c0b30eb4ed0a8c891
عبد السلام <abozbere2001@gmail.com>
2025-10-25 08:19:44 +0000

    studio-5560913388-9a637 اربط هذا المشروع بالتطبيق

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyCSTuscXA3VzGr18ADaDVGj33XGks_DY4g",
-  authDomain: "studio-3417145591-24d0a.firebaseapp.com",
-  databaseURL: "https://studio-3417145591-24d0a-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "studio-3417145591-24d0a",
-  storageBucket: "studio-3417145591-24d0a.appspot.com",
-  messagingSenderId: "731644651563",
-  appId: "1:731644651563:web:c545fe9730675b9e5f626e",
-  measurementId: "G-JLPD1C0BFH"
+  apiKey: "REPLACE_WITH_YOUR_FIREBASE_API_KEY",
+  authDomain: "studio-5560913388-9a637.firebaseapp.com",
+  databaseURL: "https://studio-5560913388-9a637-default-rtdb.firebaseio.com",
+  projectId: "studio-5560913388-9a637",
+  storageBucket: "studio-5560913388-9a637.appspot.com",
+  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
+  appId: "REPLACE_WITH_YOUR_APP_ID",
+  measurementId: "REPLACE_WITH_YOUR_MEASUREMENT_ID"
 };
