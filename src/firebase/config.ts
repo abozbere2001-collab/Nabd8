8c3496036d0e9045f65747696b015deaac96253d
عبد السلام <abozbere2001@gmail.com>
2025-10-25 08:27:33 +0000

    الان عند محاوله تسجيل الدخول عن طريق مصادقه جوجل ويظهر هذا الخطا Firebas

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_FIREBASE_API_KEY",
+  apiKey: "YOUR_API_KEY",
   authDomain: "studio-5560913388-9a637.firebaseapp.com",
   databaseURL: "https://studio-5560913388-9a637-default-rtdb.firebaseio.com",
   projectId: "studio-5560913388-9a637",
   storageBucket: "studio-5560913388-9a637.appspot.com",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID",
-  measurementId: "REPLACE_WITH_YOUR_MEASUREMENT_ID"
+  messagingSenderId: "5560913388",
+  appId: "1:5560913388:web:b1d9154f3b7543594b281b",
+  measurementId: "G-9T7B2Y7X6C"
 };
