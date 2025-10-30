42bcca8a4d6b744f880cea42326419f21316bf18
عبد السلام <abozbere2001@gmail.com>
2025-10-30 07:37:49 +0000

    اربطه بالكامل

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE", // الرجاء استبدال هذا بالمفتاح الصحيح
+  apiKey: "AIzaSyBFn_bSUTb3e_2nPSj2ODPOgT4V2mTE8vI",
   authDomain: "nabd-d71ab.firebaseapp.com",
   projectId: "nabd-d71ab",
   storageBucket: "nabd-d71ab.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "YOUR_APP_ID_HERE"
+  messagingSenderId: "1098402517133",
+  appId: "1:1098402517133:web:961817c76f9c9b7754f152"
 };
