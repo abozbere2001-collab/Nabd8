5fe2ce351e3480a9d88e4e9fe1b0e027d12d981d
عبد السلام <abozbere2001@gmail.com>
2025-10-30 07:42:03 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,11 +1,20 @@
 
 'use client';
 
-import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
+
+// Consolidating config here to ensure the correct values are always used.
+const firebaseConfig = {
+  apiKey: "AIzaSyBFn_bSUTb3e_2nPSj2ODPOgT4V2mTE8vI",
+  authDomain: "nabd-d71ab.firebaseapp.com",
+  projectId: "nabd-d71ab",
+  storageBucket: "nabd-d71ab.appspot.com",
+  messagingSenderId: "1098402517133",
+  appId: "1:1098402517133:web:961817c76f9c9b7754f152"
+};
 
 let firebaseApp: FirebaseApp;
 if (!getApps().length) {
@@ -14,9 +23,7 @@ if (!getApps().length) {
   firebaseApp = getApp();
 }
 
-const auth = getAuth(firebaseApp, {
-  authDomain: firebaseConfig.authDomain,
-});
+const auth = getAuth(firebaseApp);
 const firestore = getFirestore(firebaseApp);
 const database = getDatabase(firebaseApp);
 
