ee864fb7dce06856b0360504811761da31daf587
عبد السلام <abozbere2001@gmail.com>
2025-10-30 12:20:38 +0000

    هل تراه هل هذا هو next-env.d.ts

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // Optional: Add other configurations here if needed.
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
