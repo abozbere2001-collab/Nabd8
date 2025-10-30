c10a22f7afe49e4f46fe18645af2ff79e03bf9d7
عبد السلام <abozbere2001@gmail.com>
2025-10-30 07:37:09 +0000

    نفس المشكلة هل انت متأكد انه مربوط في قاعدة البيانات nabd-d71ab

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyDG7nLIqi2QjPqsFGLeLJFHTkrpYbx4sNk",
-  authDomain: "studio-5560913388-9a637.firebaseapp.com",
-  projectId: "studio-5560913388-9a637",
-  storageBucket: "studio-5560913388-9a637.firebasestorage.app",
-  messagingSenderId: "954115183601",
-  appId: "1:954115183601:web:e979508da0aa1de5506a50"
+  apiKey: "YOUR_API_KEY_HERE", // الرجاء استبدال هذا بالمفتاح الصحيح
+  authDomain: "nabd-d71ab.firebaseapp.com",
+  projectId: "nabd-d71ab",
+  storageBucket: "nabd-d71ab.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
+  appId: "YOUR_APP_ID_HERE"
 };
