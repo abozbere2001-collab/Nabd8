a19d363b9e6f8ea3395e56842981c068b3ba26d0
عبد السلام <abozbere2001@gmail.com>
2025-11-01 09:37:08 +0000

    !

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,6 +4,7 @@ export const LALIGA_ID = 140;
 export const SERIE_A_ID = 135;
 export const BUNDESLIGA_ID = 78;
 export const CHAMPIONS_LEAGUE_ID = 2;
-export const CURRENT_SEASON = 2025;
+export const CURRENT_SEASON = 2024;
 export const PREVIOUS_SEASON = new Date().getFullYear() - 1;
+
 
