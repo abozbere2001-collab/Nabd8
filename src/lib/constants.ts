f95cf73a562d9978ab42796f2c0816fa25fdf8b8
عبد السلام <abozbere2001@gmail.com>
2025-11-01 09:34:14 +0000

    هل الامر عناد نحن في الشهر 11 في سنه 2025

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,14 +1,9 @@
 
-
 export const PREMIER_LEAGUE_ID = 39;
 export const LALIGA_ID = 140;
 export const SERIE_A_ID = 135;
 export const BUNDESLIGA_ID = 78;
 export const CHAMPIONS_LEAGUE_ID = 2;
-export const CURRENT_SEASON = 2024;
+export const CURRENT_SEASON = 2025;
 export const PREVIOUS_SEASON = new Date().getFullYear() - 1;
-
-
-
-
 
