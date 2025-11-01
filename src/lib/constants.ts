9e837903de9f82aa9b1d9ca6c1d541c1df72bc21
عبد السلام <abozbere2001@gmail.com>
2025-11-01 15:48:30 +0000

    اعطني اكواد الدفع التعديلات الى جيت هب ولكن قبل ذلك لا تقوم بتعيين اي مو

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,8 @@ export const LALIGA_ID = 140;
 export const SERIE_A_ID = 135;
 export const BUNDESLIGA_ID = 78;
 export const CHAMPIONS_LEAGUE_ID = 2;
-export const CURRENT_SEASON = 2023;
+export const CURRENT_SEASON = 2024;
 export const PREVIOUS_SEASON = new Date().getFullYear() - 1;
+
 
 
